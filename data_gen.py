5218bdacab18e8372b688e12c3b702769502c66d
Yang Liu <foamliu@microsoft.com>
2018-10-08 22:26:18 +0800

    update

--- data_gen.py
+++ data_gen.py
@@ -3,52 +3,40 @@ import os
 import cv2 as cv
 import numpy as np
 from torch.utils.data import Dataset
-from tqdm import tqdm
 
 
-def load_data():
+def load_data(split):
     # (num_samples, 320, 320, 4)
     num_samples = 8144
     train_split = 0.8
     num_train = int(round(num_samples * train_split))
     num_valid = num_samples - num_train
 
-    x_train = np.empty((num_train, 320, 320, 3), dtype=np.float32)
-    y_train = np.empty((num_train, 320, 320, 3), dtype=np.float32)
-    x_valid = np.empty((num_valid, 320, 320, 3), dtype=np.float32)
-    y_valid = np.empty((num_valid, 320, 320, 3), dtype=np.float32)
+    if split == 'train':
+        num_samples = num_train
+        folder = 'data/train'
+    else:
+        num_samples = num_valid
+        folder = 'data/valid'
 
-    i_train = i_valid = 0
-    for root, dirs, files in tqdm(os.walk("data", topdown=False)):
-        for name in files:
-            filename = os.path.join(root, name)
-            bgr_img = cv.imread(filename)
-            rgb_img = cv.cvtColor(bgr_img, cv.COLOR_BGR2RGB)
-            if filename.startswith('data/train'):
-                x_train[i_train, :, :, :] = rgb_img / 255.
-                y_train[i_train, :, :, :] = rgb_img / 255.
-                i_train += 1
-            elif filename.startswith('data/valid'):
-                x_valid[i_valid, :, :, :] = rgb_img / 255.
-                y_valid[i_valid, :, :, :] = rgb_img / 255.
-                i_valid += 1
+    x = np.empty((num_samples, 320, 320, 3), dtype=np.float32)
+    y = np.empty((num_samples, 320, 320, 3), dtype=np.float32)
 
-    return x_train, y_train, x_valid, y_valid
+    files = [os.path.join(folder, f) for f in os.listdir(folder) if f.endswith('.jpg')]
 
+    for i, filename in enumerate(files):
+        bgr_img = cv.imread(filename)
+        rgb_img = cv.cvtColor(bgr_img, cv.COLOR_BGR2RGB)
+        x[i, :, :, :] = rgb_img / 255.
+        y[i, :, :, :] = rgb_img / 255.
 
-x_train, y_train, x_valid, y_valid = load_data()
+    return x, y
 
 
 class VaeDataset(Dataset):
     def __init__(self, split):
         self.split = split
-
-        if split == 'train':
-            self.x = x_train
-            self.y = y_train
-        else:
-            self.x = x_valid
-            self.y = y_valid
+        self.x, self.y = load_data(split)
 
     def __getitem__(self, i):
         return self.x[i], self.y[i]
